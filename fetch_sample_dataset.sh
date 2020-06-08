1b14d414c528f07d335f3479f47bc960f2a8d7f8
Manu Flores <emanuelflrsb@hotmail.com>
2020-06-08 01:17:59 -0700

    Added simple script to download a single fish dataset from the Janelia repo.

---
+++ fetch_sample_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p data_/;
+wget -O data_/fish_10_data.zip https://janelia.figshare.com/ndownloader/files/13481963;
+wget -O data_/additional_files.zip https://janelia.figshare.com/ndownloader/files/13526369;
+
